b20fa19c37d4623ee9b3897c6f69460d9af6e46f
visualstudio <visualstudio@122-PC25>
2019-04-17 10:29:25 -0400

    longestword

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -6,10 +6,25 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp2
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
+    
+        
+            static void Main(string[] args)
+            {
+                Program a = new Program();
+                Console.WriteLine("the longest world is :");
+                Console.WriteLine(a.MyFunctionA("ada lovelace wrote the first algorith desingned for processing bt an Analytical Engine."));
+            }
+
+            private bool MyFunctionA(string v)
+            {
+                throw new NotImplementedException();
+            }
+
+            public MyFunctionA(string input)
+            {
+                // Write a C# prograam to find the longest word in a string.
+                string[] words = input.Split('');
+
+            }
         }
-    }
 }
